fd95874273d0e74eda63dc18c1106641dc3a2103
Melos Shabi <melosshabi@protonmail.com>
2026-04-30 09:45:57 +0200

    Updated the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,23 @@
 import { MetadataRoute } from "next";
-
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://pulse.velovix.com",
-      lastModified: new Date("2026-04-26"),
+      lastModified: new Date("2026-04-30"),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: "https://pulse.velovix.com/pricing",
-      lastModified: new Date("2026-04-26"),
+      lastModified: new Date("2026-04-30"),
       changeFrequency: "monthly",
       priority: 0.8,
+    },
+    {
+      url: "https://pulse.velovix.com/docs",
+      lastModified: new Date("2026-04-30"),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
     {
       url: "https://pulse.velovix.com/login",
