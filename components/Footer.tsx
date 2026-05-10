a866b6df2b22e4685b489666027c958ed4f11efc
Melos Shabi <melosshabi@protonmail.com>
2026-05-11 01:25:40 +0200

    Added Bluesky link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,6 +8,7 @@ import {
   LinkedinLogoIcon,
   GithubLogoIcon,
 } from "@phosphor-icons/react/dist/ssr";
+import bluesky from "@/images/bluesky.svg";
 
 export default function Footer() {
   return (
@@ -47,6 +48,28 @@ export default function Footer() {
               className="flex items-center justify-center w-8 h-8 rounded-lg text-text-muted hover:text-accent hover:bg-accent/10 transition-all duration-200"
             >
               <LinkedinLogoIcon size={20} />
+            </Link>
+            <Link
+              target="_blank"
+              href={"https://bsky.app/profile/thevelovix.bsky.social"}
+              className="flex items-center justify-center w-8 h-8 rounded-lg text-text-muted hover:text-accent hover:bg-accent/10 transition-all duration-200"
+            >
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                height="18"
+                width="18"
+              >
+                <title>Bluesky</title>
+                <path
+                  d="M12 10.8c-1.087 -2.114 -4.046 -6.053 -6.798 -7.995C2.566 0.944 1.561 1.266 0.902 1.565 0.139 1.908 0 3.08 0 3.768c0 0.69 0.378 5.65 0.624 6.479 0.815 2.736 3.713 3.66 6.383 3.364 0.136 -0.02 0.275 -0.039 0.415 -0.056 -0.138 0.022 -0.276 0.04 -0.415 0.056 -3.912 0.58 -7.387 2.005 -2.83 7.078 5.013 5.19 6.87 -1.113 7.823 -4.308 0.953 3.195 2.05 9.271 7.733 4.308 4.267 -4.308 1.172 -6.498 -2.74 -7.078a8.741 8.741 0 0 1 -0.415 -0.056c0.14 0.017 0.279 0.036 0.415 0.056 2.67 0.297 5.568 -0.628 6.383 -3.364 0.246 -0.828 0.624 -5.79 0.624 -6.478 0 -0.69 -0.139 -1.861 -0.902 -2.206 -0.659 -0.298 -1.664 -0.62 -4.3 1.24C16.046 4.748 13.087 8.687 12 10.8Z"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinejoin="round"
+                />
+              </svg>
             </Link>
           </div>
         </div>
